8086d0ec935d3cac03122ec8a39bcf39f0633d80
Bob Sira <bosira@microsoft.com>
2021-01-09 01:44:23 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, sirasira!
+          Hello, sirasira! This happened on the feature branch
         </p>
         <a
           className="App-link"
